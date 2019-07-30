e1aca34b8272804da16031004f25113f84359b49
LGM - ComIT <lucascn21@gmail.com>
2019-07-30 10:41:41 -0300

    Agregado practica de arrays, como inicializarlos, algunas funciones comunes

---
+++ practicaJS/arrays.js
@@ -0,0 +1,16 @@
+var pais = ['Mexico', 'España', 'Argentina', 'Chile', 'Colombia', 'Venezuela', 'Perú', 'Costa Rica'];
+/*
+En este ejemplo decimos que hemos declarado un array de 8 elementos.
+ Cada elemento tiene un índice, comenzando los índices por el número 0. 
+ Por tanto los 8 elementos del array anterior son:
+pais[0], pais[1], pais[2], pais[3], pais[4], pais[5], pais[6] y pais[7].
+También podemos inicializar un array vacío de dos formas distintas:
+*/
+var fruta = [];
+
+var fruta = new Array();
+
+
+pais.length;
+pais.splice(" ");
+pais.join("---")
