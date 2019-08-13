a81d40d9bbee43435c9508d84e443bb2503d8ffe
LGM - ComIT <lucascn21@gmail.com>
2019-08-13 10:46:45 -0300

    mas practica

--- practicaJS/funciones.js
+++ practicaJS/funciones.js
@@ -1,11 +1,25 @@
+saludar(); //Retorna undefined
+suma(2,5); //Funciona, retorna la suma de los numeros, tambien los muestra por consola
+for(let i=0; i<5; i++){
+
+    saludar("cacho  "); //Funciona
+}
+
+
+
+
+
+
+
 function saludar(nombre){ //El parametro recibido se llamara como se declare en esta parte
 console.log(`hola ${nombre}\n`);
 
 }
 
 
-saludar(); //Retorna undefined
-for(let i=0; i<5; i++){
 
-    saludar("cacho  "); //Funciona
+function suma(num1, num2){
+    console.log(`${num1}+${num2}=${num1+num2}`);
+    return num1+num2;
+
 }
