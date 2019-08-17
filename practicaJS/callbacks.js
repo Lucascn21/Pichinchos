759241f67c292668dea5b9df67c36b80d5df76f6
LGM - ComIT <lucascn21@gmail.com>
2019-08-17 16:55:24 -0300

    Fin de callbacks.
    TODO contenido extra de la clase

--- practicaJS/callbacks.js
+++ practicaJS/callbacks.js
@@ -1,6 +1,9 @@
 /**
  * Se conoce como CALLBACK cuando pasamos una funcion como parametro a otra funcion
  * Dado que ECMASCript es asyncronico, necesitamos una forma de saber si algo se termino de ejecutar o no
+ * Este concepto es fundamental en ECMAScript, lo vamos a seguir viendo al utilizar JS tanto en browser como en node
+ *  Utilizando CALLBACKS es una de las varias formas que hay de hacer que nuestro codigo se ejecute
+ *  como si fuera sincronico cuando no lo es.
  */
 
 //Ejemplo
@@ -39,3 +42,39 @@ function sumar(n, callback) {
 
 sumar(numero, resultadoSuma); //1
 console.log(numero);          //0
+
+
+//mas ejemplos
+let numeros = [1, 2, 3, 4, 5, 7, 8, 9, 10];
+
+
+//Callback
+console.log("\n");
+numeros.forEach(function (numero) {
+    console.log(numero);
+});
+//Callback
+let numerosPares = numeros.filter(function (numero) {
+    return numero % 2 === 0;
+});
+//Callback
+let resultado = numeros.reduce(function (resultado, numero) {
+    return resultado + numero;
+})
+//Callback
+let numerosModificados = numeros.map(function (numero) {
+    return numero + 10;
+})
+
+/**
+ * Arriba estamos usando metodos de Arrays
+ *
+ * El primer metodo, al que le pasamos un callback, es un foreach al que le damos
+ * una funcion anonima por parametro, la cual a su vez recibe por parametros a los elementos del array
+ *
+ * El segundo, filtra y devuelve true o false, por ende serian true los pares
+ *
+ * El tercero, reduce, recibe por parametros el numero anterior y el numero por el que se esta iterando dentro del array, en este caso retorna la suma total de elementos
+ *
+ * El cuarto retorna un array pero le suma 10 a cada elemento.
+ */
