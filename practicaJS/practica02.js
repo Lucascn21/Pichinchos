3b295d4da08aefde8bab5bd239be14491f1e555d
LGM - ComIT <lucascn21@gmail.com>
2019-08-22 12:08:59 -0300

    Estilado de codigo

--- practicaJS/practica02.js
+++ practicaJS/practica02.js
@@ -1,5 +1,15 @@
-var listOriginal =[12,6,8,9,5,7,];
+var listOriginal = [12, 6, 8, 9, 5, 7,];
 
-var listaModificada = miMap(listaOriginal, function(valor){
+var listaModificada = miMap(listaOriginal, function (valor) {
     return valor * 2;
+
+
+    function miMap(lista, cbTransform) {
+        let listaNueva = [];
+
+        for (let i = 0; i < lista.length; i++) {
+            listaNueva.push(cbTransform(lista[i, i, lista]));
+        }
+        return listaNueva;
+    }
 });
