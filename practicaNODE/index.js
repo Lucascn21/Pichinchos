7db947ae65a7eb0c4bfc3c897ca069cb66d2de31
LGM - ComIT <lucascn21@gmail.com>
2019-09-05 12:50:34 -0300

    Usamos FS en Node para leer un JSON y mostrarlo por consola.
     Hablamos un poco de asincronia respecto

--- practicaNODE/index.js
+++ practicaNODE/index.js
@@ -1,2 +1,19 @@
 //forma en la que vamos a importar librerias, sea que las bajamos con npm o las tenemos por ahi. Tambien sirve para vincular cualquier otro archivo.
-require
+const fs = require('fs');
+//console.log(fs);
+var path = "test.json";
+var data = fs.readFileSync(path);
+/*
+console.log(data);
+console.log(JSON.parse(data));
+*/
+
+//Es una buena regla de dedo que si veo un CALLBACK, TENGO QUE CONSIDERAR LA ASINCRONIA A LA HORA DE MANEJAR LOS DATOS EN MI HILO DE CODIGO
+fs.readFile(path, (err, data) => { //Si hago otro readfile paralelo, el que tarde mas va a venir segundo, mas alla de cuando fue ejecutado, recordar callbacks y cb queue
+    if (err) {
+        console.log("¡ERROR! \n" + err)
+    } else {
+        let obj = JSON.parse(data);
+        console.log(obj);
+    }
+});
