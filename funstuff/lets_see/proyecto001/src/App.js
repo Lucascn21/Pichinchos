08b129cfa7499ae608fcedd4ea638d02b546e0b0
LGM - ComIT <lucascn21@gmail.com>
2019-09-12 09:18:51 -0300

    Mas aprendizaje de React

--- funstuff/lets_see/proyecto001/src/App.js
+++ funstuff/lets_see/proyecto001/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Alert } from 'reactstrap';
 
 //Recordar que instale https://reactstrap.github.io
 class App extends Component {
   //El método render() tiene por objetivo generar la interfaz visual de la componente 'App'
+  constructor(props) {
+    super(props)
+    this.presion = this.presion.bind(this);
+    this.generarAleatorio = this.generarAleatorio.bind(this);
+    //Podemos definir en state todos los datos que van a variar durante la ejecución de la aplicación y queremos que se actualicen en pantalla.
+    this.state = {
+      numero: 'No se ha generado número aleatorio aún'
+    }
+  }
   render() {
     const siglo=21;
     const persona = {
@@ -15,7 +23,7 @@ class App extends Component {
     return (
       <div>
       <Alert color="primary">
-        This is a primary alert with <a href="#" className="alert-link">an example link</a>. Give it a click if you like.
+        This is a primary alert with <a href="\logo192.png" className="alert-link">an example link</a>. Give it a click if you like.
       </Alert>
         <h1>Título nivel 1</h1>
         <h2>{this.tit="bla2"}</h2>
@@ -30,9 +38,24 @@ class App extends Component {
         {this.retornarAleatorio()}
         <h3>Calculo inmediato de expresiones</h3>
         3 + 3 = {3+3}
+     
+        <form onSubmit={this.presion}>
+        <p>Ingrese primer valor:
+          <input type="number" name="valor1" />
+        </p>
+        <p>Ingrese segundo valor:
+          <input type="number" name="valor2" />
+        </p>        
+        <p>
+          <input type="submit" value="Sumar" />
+        </p>
+        </form>
+
+        <p>Número aleatorio: {this.state.numero}</p>
+        <button onClick={this.generarAleatorio}>Generar número aleatorio</button>
       </div>
     );
-  }
+  } 
 
   retornarAleatorio() {
     return Math.trunc(Math.random() * 10);
@@ -41,6 +64,21 @@ class App extends Component {
     return (<h1>
              {tit}
             </h1>)
+  }
+  presion(e) {
+    e.preventDefault(); //evita la accion default de refrescar la pagina
+    const v1=parseInt(e.target.valor1.value, 10);
+    const v2=parseInt(e.target.valor2.value, 10);
+    const suma=v1+v2;
+    alert('La suma es: '+suma);
+  }
+  generarAleatorio() {
+    const v=Math.trunc(Math.random()*10);
+    this.setState( {
+      numero: v,
+    })
+    //alert("no se por que modifica el <p> que es el retorno de una funcion que retorna un random")
+    //mediante la llamada al método setState (que se hereda de la clase Component) modificamos su valor, con esto la librería React se encarga de ejecutar nuevamente el método render() pero solo actualizando los estados cambiados y sin tener que redibujar la página completa.
   }
 }
 
