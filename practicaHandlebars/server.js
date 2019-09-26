714d8e4f938580eb1b3eb74f855071a8e0095669
Lucascn21 <lucascn21@gmail.com>
2019-09-26 16:33:01 -0300

    Mejorado el ejercicio de handlebars

--- practicaHandlebars/server.js
+++ practicaHandlebars/server.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const exphbs = require('express-handlebars');
 
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs({
@@ -19,11 +18,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/paises", (req, res) => {
-
     fs.readFile(path.join(__dirname, "resources/paises.json"), (err, data) => {
         if (!err) {
             let paises = JSON.parse(data);
-            console.dir(req.query.nombre);
+            console.dir(req.query.nombre); //Confirmo que llegan queries
             console.dir(req.query.codigo);
 
             if (req.query.nombre) {
@@ -33,40 +31,41 @@ app.get("/paises", (req, res) => {
             if (req.query.codigo) {
                 paises = paises.filter(pais => pais.code == req.query.codigo);
             }
-            console.dir(paises);
+            console.dir(paises); //Confirmo que el array filtrado cumple con lo esperado
             res.render('paises', {
-                listaPaises: paises
+                listaPaises: paises,
+                tipoMetodo: req.method //Es para mostrar dinamicamente que tipo de metodo se usó en el H2 de la view paises.
             });
         }
     });
 
 });
 
-/*
-const queryParser = require('query-parser');
-app.use(queryParser.urlencoded({ extended: true }));
-app.post("/paises", (req, res) => {
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); //segun tipo de info que viene en el form, puede ser que nos genere problemas sin extended true. Es un objeto nuevo.
+
+app.post("/paises", (req, res) => {
     fs.readFile(path.join(__dirname, "resources/paises.json"), (err, data) => {
         if (!err) {
             let paises = JSON.parse(data);
-            console.dir(req.query);
-            if (req.query.nombre) {
-                paises = paises.filter(pais => pais.name == req.query.nombre);
+            console.dir(req.body); //confirmo el body de la request
+            if (req.body.nombre) {
+                paises = paises.filter(pais => pais.name == req.body.nombre);
             }
 
-            if (req.query.codigo) {
-                paises = paises.filter(pais => pais.code == req.query.codigo);
+            if (req.body.codigo) {
+                paises = paises.filter(pais => pais.code == req.body.codigo);
             }
-            console.dir(paises);
+            console.dir(paises); //confirmo se filtro correctamente
             res.render('paises', {
-                listaPaises: paises
+                listaPaises: paises,
+                tipoMetodo: req.method ////Es para mostrar dinamicamente que tipo de metodo se usó en el H2 de la view paises.
             });
         }
     });
-
 });
-*/
+
 app.listen(3000, () => {
     console.log("Corriendo en puerto 3000 http://localhost:3000/");
 });
